3b5dd861c247ffaa6d6fe461c1d1ce962ac8c0fe
tuxmaster <tuxmaster@users.noreply.github.com>
2022-03-05 14:35:53 +0100

    Start

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+/*
+	Copyright (C) 2019 Frank Büttner tuxmaster@users.noreply.github.com
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+#include <QtCore/qglobal.h>
+#if QT_VERSION >= 0x050000
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
+#else
+#endif
+
+int main(int argc, char *argv[])
+{
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+	QGuiApplication app(argc, argv);
+
+	QQmlApplicationEngine engine;
+	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+					 &app, [url](QObject *obj, const QUrl &objUrl) {
+		if (!obj && url == objUrl)
+			QCoreApplication::exit(-1);
+	}, Qt::QueuedConnection);
+	engine.load(url);
+
+	return app.exec();
+}
